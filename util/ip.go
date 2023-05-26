54b83faa1b7edb06681c27898cbe3c78cf572357
gokingliu <921229265@qq.com>
2023-05-27 03:08:27 +0800

    fix: 获取ipv6

--- util/ip.go
+++ util/ip.go
@@ -13,10 +13,9 @@ func GetIPv4() string {
 	// 执行 Linux 命令
 	result, err := exec.Command("/bin/sh", "-c", IPv4LinuxCommand).Output()
 	// 执行错误返回空字符串
-	if err != nil {
+	if err != nil || string(result) == "" {
 		// 执行 OpenWRT 命令
 		result, _ = exec.Command("/bin/sh", "-c", IPv4OpenWRTCommand).Output()
-		return ""
 	}
 	// 读取命令结果中的文本z
 	IPv4 := strings.TrimSpace(string(result))
@@ -27,15 +26,14 @@ func GetIPv4() string {
 // GetIPv6 获取 IPv6 地址
 func GetIPv6() string {
 	// 读取 IPv6 命令
-	IPv6LinuxCommand := "ifconfig -a | grep 'inet6 [^f:]' | grep 'temporary' | awk '{print $2}'| tr -d 'addr:'"
-	IPv6OpenWRTCommand := `ip -o addr show | grep -E 'eth|en' | grep -v deprecated | grep 'inet6 [^f:]' | sed -nr 's#^.+? +inet6 ([a-f0-9:]+)/.+? scope global .*? valid_lft ([0-9]+sec) .*#\2 \1#p' | grep 'ff:fe'| sort -nr | head -n1 | cut -d' ' -f2`
+	IPv6LinuxCommand := "ifconfig -a | grep 'inet6 [^f:]' | grep -E 'temporary|global' | awk '{print $2}'| tr -d ''"
+	IPv6OpenWRTCommand := `ip -o addr show | grep -E 'eth|en' | grep -v deprecated | grep 'inet6 [^f:]' | sed -nr 's#^.+? +inet6 ([a-f0-9:]+)/.+? scope global .*? valid_lft ([0-9]+sec) .*#\2 \1#p' | sort -nr | head -n1 | cut -d' ' -f2`
 	// 执行命令
 	result, err := exec.Command("/bin/sh", "-c", IPv6LinuxCommand).Output()
 	// 执行错误返回空字符串
-	if err != nil {
+	if err != nil || string(result) == "" {
 		// 执行 OpenWRT 命令
 		result, _ = exec.Command("/bin/sh", "-c", IPv6OpenWRTCommand).Output()
-		return ""
 	}
 	// 读取命令结果中的文本
 	IPv6 := strings.TrimSpace(string(result))

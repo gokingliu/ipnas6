b3f7cfdb0ce6e3acf5732acec89645c4452090f0
gokingliu <921229265@qq.com>
2023-05-27 19:11:48 +0800

    fix: 文件编码

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-
-# 后端服务文件
-BACKEND_FILE=/app/ipnas6
-
-# 启动后端服务
-nohup ${BACKEND_FILE} > /dev/null 2>&1 &
+#!/bin/sh# 后端服务文件BACKEND_FILE=/app/ipnas6# 启动后端服务nohup ${BACKEND_FILE} > /dev/null 2>&1 &
